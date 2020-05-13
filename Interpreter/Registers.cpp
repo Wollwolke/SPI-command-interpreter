a04f9c0aed28171416dc0c41361c71c60549f723
Paul Pflugradt <paul.pflugradt@student.fhws.de>
2020-05-13 12:35:37 +0200

    projekt eingefügt

---
+++ Interpreter/Registers.cpp
@@ -0,0 +1,60 @@
+#include "Registers.h"
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+
+Registers::Registers() {}
+
+Registers::Register::Register(std::vector<std::string> wNames, std::vector<std::string> rNames, std::vector<int> dValues) {
+	for (size_t i = 0; i < Registersize; i++)
+	{
+		Values[i] = dValues[i];
+		NameIndices.insert({ wNames[i], i });
+		NameIndices.insert({ rNames[i], i });
+	}
+};
+
+void Registers::Register::writeRegister(uint8_t bits[Registersize]) {
+	for (size_t i = 0; i < Registersize; i++)
+	{
+		Values[i] = bits[i];
+	}
+}
+
+int Registers::Register::readBit(std::string name) {
+	if (NameIndices.count(name) != 0) {
+		return Values[NameIndices[name]];
+	}
+	return -1;
+}
+
+void Registers::createRegisters(std::string fname) {
+	std::ifstream file(fname);
+	nlohmann::json jfile;
+	file >> jfile;
+	auto test = jfile.at("registers");
+	for (auto& element : test)
+	{
+		//std::cout << i.value << "\n";
+		try {
+			std::string name = element.at("name");
+			std::vector<int> values = element.at("values");			
+			std::vector<std::string> write = element.at("w");
+			std::vector<std::string> read = element.at("r");
+			Register* newReg = new Register(write, read, values);
+
+			regMap.insert({ name, newReg });
+			//std::cout << name << ++cnt << "\n";
+		}
+		catch (nlohmann::detail::out_of_range exe) {
+			std::cout << "json wrong format";
+			break;
+		}
+	}
+}
+
+Registers::~Registers() {
+	for (const auto& [key, value]: regMap) {
+		delete value;
+	}
+}
---
+++ Interpreter/Registers.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+
+class Registers
+{
+private:
+	static const int Registersize = 8;
+	class Register {
+	public:
+		std::map<std::string, uint8_t> NameIndices;
+		int Values[Registersize];
+
+		Register(std::vector<std::string> wNames, std::vector<std::string> rNames, std::vector<int> dValues);
+
+		void writeRegister(uint8_t bits[Registersize]);
+
+		int readBit(std::string name);
+	};
+	std::map<std::string, Register*> regMap;
+public:
+	Registers();
+	void createRegisters(std::string fname);
+	~Registers();
+};
