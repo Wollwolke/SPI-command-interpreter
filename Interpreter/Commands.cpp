3c33dc96115ba94c123be4591fd8dcb5bb048e50
Paul Pflugradt <paul.pflugradt@student.fhws.de>
2020-05-13 13:47:15 +0200

    implemented cmd clas

--- Interpreter/Commands.cpp
+++ Interpreter/Commands.cpp
@@ -1,5 +1,24 @@
 #include "Commands.h"
 
-Commands::Commands()
+Commands::Commands(nlohmann::json& config)
 {
+	auto it = config.at("commands");
+	for (auto& command : it) {
+		Command* nextcmd;
+		if (command.at("isstrobe")) {
+			nextcmd = new StrobeCommand(command.at("logic"));
+		}
+		else {
+			nextcmd = new RegCommand(command.at("isread"), command.at("registername"));
+		}
+		cmds.insert({ command.at("hex"), nextcmd });
+	}
 }
+
+
+Commands::Command::Command(bool strobe) :isstrobe(strobe) {}
+
+Commands::StrobeCommand::StrobeCommand(std::string ilogic) :interpretation(ilogic), Command(true) {}
+
+Commands::RegCommand::RegCommand(bool isread, std::string name):isread(isread),registername(name), Command(false){}
+
--- Interpreter/Commands.h
+++ Interpreter/Commands.h
@@ -1,22 +1,29 @@
 #pragma once
 #include <map>
+#include <nlohmann/json.hpp>
+#include <string>
 
 class Commands
 {
 public:
-	Commands();
-private:
+	Commands(nlohmann::json& config);
+protected:
 	class Command {
-	public:
 		bool isstrobe;
+	public:
+		Command(bool strobe);
 	};
-	class Strobecommand : public Command {
+	class StrobeCommand : public Command {
+	public:
 		std::string interpretation;
+		StrobeCommand(std::string ilogic);
 	};
 	class RegCommand : public Command {
+	public:
 		bool isread;
 		std::string registername;
+		RegCommand(bool isread, std::string name);
 	};
-	std::map<std::string, Command> cmds;
+	std::map<std::string, Command*> cmds;
 };
 
