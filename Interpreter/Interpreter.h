1844b8782e35c69ece749f7e24bd511a4df9c5a1
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2020-05-13 13:45:54 +0200

    added interpreter class

--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "Registers.h"
+#include "Commands.h"
+#include <string>
 class Interpreter
 {
-};
+private:
+	Registers registers;
+	Commands commands;
 
+public:
+	Interpreter(std::string confName);
+	void interpretFile(std::string fName);
+};
--- Interpreter/Registers.cpp
+++ Interpreter/Registers.cpp
@@ -5,56 +5,61 @@
 
 Registers::Registers() {}
 
-Registers::Register::Register(std::vector<std::string> wNames, std::vector<std::string> rNames, std::vector<int> dValues) {
+Registers::Register::Register(std::vector<std::string> wNames, std::vector<std::string> rNames, std::vector<int> dValues)
+{
 	for (size_t i = 0; i < Registersize; i++)
 	{
 		Values[i] = dValues[i];
-		NameIndices.insert({ wNames[i], i });
-		NameIndices.insert({ rNames[i], i });
+		NameIndices.insert({wNames[i], i});
+		NameIndices.insert({rNames[i], i});
 	}
 };
 
-void Registers::Register::writeRegister(uint8_t bits[Registersize]) {
+void Registers::Register::writeRegister(uint8_t bits[Registersize])
+{
 	for (size_t i = 0; i < Registersize; i++)
 	{
 		Values[i] = bits[i];
 	}
 }
 
-int Registers::Register::readBit(std::string name) {
-	if (NameIndices.count(name) != 0) {
+int Registers::Register::readBit(std::string name)
+{
+	if (NameIndices.count(name) != 0)
+	{
 		return Values[NameIndices[name]];
 	}
 	return -1;
 }
 
-void Registers::createRegisters(std::string fname) {
-	std::ifstream file(fname);
-	nlohmann::json jfile;
-	file >> jfile;
-	auto test = jfile.at("registers");
-	for (auto& element : test)
+void Registers::createRegisters(nlohmann::json &jregisters)
+{
+	for (auto &element : jregisters)
 	{
 		//std::cout << i.value << "\n";
-		try {
+		try
+		{
 			std::string name = element.at("name");
-			std::vector<int> values = element.at("values");			
+			std::vector<int> values = element.at("values");
 			std::vector<std::string> write = element.at("w");
 			std::vector<std::string> read = element.at("r");
-			Register* newReg = new Register(write, read, values);
+			Register *newReg = new Register(write, read, values);
 
-			regMap.insert({ name, newReg });
+			regMap.insert({name, newReg});
 			//std::cout << name << ++cnt << "\n";
 		}
-		catch (nlohmann::detail::out_of_range exe) {
+		catch (nlohmann::detail::out_of_range exe)
+		{
 			std::cout << "json wrong format";
 			break;
 		}
 	}
 }
 
-Registers::~Registers() {
-	for (const auto& [key, value]: regMap) {
+Registers::~Registers()
+{
+	for (const auto &[key, value] : regMap)
+	{
 		delete value;
 	}
 }
