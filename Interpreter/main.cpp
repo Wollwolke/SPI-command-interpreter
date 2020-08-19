846083612f8ce4a3f3017fe807b7374c6944896f
Paul Pflugradt <Paul.Pflugradt@student.fhws.de>
2020-08-19 15:55:57 +0200

    added hardcoded all presenter 1

--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -8,6 +8,9 @@ int main()
 		auto interpreter = Interpreter("Registers.json");
 		interpreter.interpretFile("B01_power_on.txt");
 		interpreter.interpretFile("A02_button_up.txt");
+		interpreter.interpretFile("A03_button_down.txt");
+		interpreter.interpretFile("B04_button_tab.txt");
+		interpreter.interpretFile("B05_button_enter.txt");
 	}
 	catch (ERRORCODES e) {
 
