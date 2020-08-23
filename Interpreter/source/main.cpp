6906cf33fd98a73dcad74f2580776a65f096d987
Paul Pflugradt <Paul.Pflugradt@student.fhws.de>
2020-08-23 16:47:22 +0200

    bug foud and fixed

--- Interpreter/source/main.cpp
+++ Interpreter/source/main.cpp
@@ -6,7 +6,6 @@
 
 int main(int argc, char* argv[])
 {
-	/*
 	if (argc == 1) {
 		std::cout << "+++++no command line argmuments given the txt logfiles in the data folder will be recursively analysed this can lead to wrong outputs due to wrong execution order of for example startup files+++++\n";
 		auto interpreter = Interpreter("./data/Registers.json");
@@ -29,17 +28,17 @@ int main(int argc, char* argv[])
 	}
 
 	std::cout << "\npress any key to continue";
-	std::cin.get();*/
+	std::cin.get();
 
-	try
-	{
-		auto interpreter = Interpreter("../data/Registers.json");
-		interpreter.interpretFile("../data/presenter2/B05_key_audioLowerVolume.txt");
+	//try
+	//{
+	//	auto interpreter = Interpreter("../data/Registers.json");
+	//	interpreter.interpretFile("../data/presenter2/B05_key_audioLowerVolume.txt");
 	//	interpreter.interpretFile("../data/presenter1/A02_button_up.txt");
 	//	interpreter.interpretFile("../data/presenter1/A03_button_down.txt");
 	//	interpreter.interpretFile("../data/presenter1/B04_button_tab.txt");
 	//	interpreter.interpretFile("../data/presenter1/B05_button_enter.txt");
-	}
-	catch (ERRORCODES)
-	{}
+	//}
+	//catch (ERRORCODES)
+	//{}
 }
